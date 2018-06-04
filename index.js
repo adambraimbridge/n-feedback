6fd110e0505986713128d1100f238fb824b62f25
Mehmet Duran <mehmet.duran@gmail.com>
2018-06-04 05:49:54 +0100

    wip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Overlay = require('o-overlay');
+const surveyBuilder = require('./survey-builder');
 
 async function getSurveyData (){
 	const surveyDataURL = 'http://local.ft.com:5005/public/survey.json';
@@ -9,131 +10,14 @@ async function getSurveyData (){
 	});
 }
 
-function buildHeader (question){
-	return `<h3 class="feedback__question-header">${question.questionText}</h3>`;
-}
-
-function buildText (question){
-	return `<p class="feedback__question-text">${question.questionText}</p>`;
-}
-
-function buildFooter (question){
-	return `<p class="feedback__question-footer">${question.questionText}</p>`;
-}
-
-function buildQuestion (question){
-	const questionBuilderMap = {
-		// MC - Multiple Choice
-		'MC': buildMultipleChoiceQuestion,
-		// TE - Text Entry
-		'TE': buildTextEntryQuestion
-	};
-
-	const questionType = question.questionType.type;
-	if( questionType in questionBuilderMap ){
-		return questionBuilderMap[questionType](question);
-	}else{
-		console.log('Feedback: buildQuestion - question type not defined: ', questionType, question);
-	}
-
-	return '';
-}
-
-function buildMultipleChoiceQuestion (question){
-	const html = [`<fieldset class="feedback__question-radio o-forms">
-									<legend>${question.questionText}</legend>
-									<div class="feedback__question-radio__container o-forms__group">`]; // fieldsets can't display: flex
-
-	Object.entries(question.choices).forEach( ([choiceId, choice], current, choices) => {
-		let textVisibility = '';
-		// if not the first or the last element
-		if( !(choiceId === choices[0][0] || choiceId === choices[choices.length-1][0]) ){
-			textVisibility = 'hidden-label';
-		}
-		const fieldId = `choice-${choiceId}-${~~(Math.random()*0xffff)}`;
-
-		html.push(`<div class="feedback__question-radio__choice-container">
-								<input type="radio" id="${fieldId}" class="o-forms__radio" name="${question.questionId}" value="${choiceId}" />
-								<label for="${fieldId}" class="o-forms__label ${textVisibility}">
-									<span class="feedback__question-radio-text">${choice.choiceText}</span>
-								</label>
-							</div>`);
-	});
-
-	html.push(`</div>
-						</fieldset>`);
-	return html.join('\n');
-}
-
-function buildTextEntryQuestion (question){
-	const fieldId = `text-${question.questionId}-${~~(Math.random()*0xffff)}`;
-
-	const html = `<p class="feedback__question-text-entry o-forms">
-									<label for="${fieldId}" class="o-forms__label">${question.questionText}</label>
-									<textarea id="${fieldId}" name="${question.questionId}" class="o-forms__textarea"></textarea>
-								</p>`;
-
-	return html;
-}
-
-function buildSurveyHTML (surveyData){
-	const builderMap = {
-		header: buildHeader,
-		footer: buildFooter,
-		text: buildText,
-		question: buildQuestion
-	};
-
-	const surveyHTML = ['<div class="feedback__survey">'];
-
-	surveyData.forEach( (block, blockId) => {
-		// Only show the first block initially, hide the others
-		const hiddenClass = (blockId === 0 ? '': 'hidden');
-		const blockHTML = [`<div class="feedback__survey-block ${hiddenClass} feedback__survey-block-${blockId}">`];
-
-		block.questions.forEach( question => {
-			const blockType = question.questionName.toLowerCase();
-			if( blockType in builderMap ){
-				const questionHTML = builderMap[blockType]( question );
-				blockHTML.push(questionHTML);
-			}else{
-				console.log('Feedback: Qualtrics survey question type not defined: ', blockType, question);
-			}
-		});
-
-		// if this not the last block on the survey
-		if( surveyData.length - 1 > blockId ){
-			blockHTML.push(`<p class="feedback__survey__button-bar">
-												<button class="o-buttons o-buttons--primary feedback__survey-next"
-																data-survey-next="feedback__survey-block-${blockId+1}">
-													Next
-												</button>
-											</p>`
-			);
-		}else{
-			blockHTML.push(`<p class="feedback__survey__button-bar">
-												<button class="o-buttons o-buttons--primary feedback__survey-submit">
-													Submit
-												</button>
-											</p>`
-			);
-		}
-
-		blockHTML.push('</div>');
-		surveyHTML.push(blockHTML.join('\n'));
-	});
-
-	surveyHTML.push('</div>');
-	return surveyHTML.join('\n');
-}
-
-
 function setBehaviour (overlay){
 	const context = overlay.content;
 
 	const nextButtons = document.querySelectorAll('.feedback__survey-next', context);
 	nextButtons.forEach( button => {
 		button.addEventListener('click', event => {
+			event.preventDefault();
+
 			const current = event.target;
 			const block = current.closest('.feedback__survey-block');
 			const nextBlockSelector = '.' + current.getAttribute('data-survey-next');
@@ -149,14 +33,30 @@ function setBehaviour (overlay){
 		}, true);
 	});
 
-
 	const submitButtons = document.querySelectorAll('.feedback__survey-submit', context);
 	submitButtons.forEach( button => {
 		button.addEventListener('click', event => {
+			event.preventDefault();
+
 			const current = event.target;
+
+			const request = new XMLHttpRequest();
+			request.open("POST", "/formlol");
+			request.send(generateResponse(overlay));
 			overlay.close();
 		});
 	});
+}
+
+function generateResponse(overlay){
+	const context = overlay.content;
+	const form = document.querySelector('.feedback__survey__wrapper-form', context);
+	const response = {};
+	(new FormData(form)).forEach((val, key) => {
+		response[key] = val;
+	});
+
+	return JSON.stringify(response);
 }
 
 function toggleOverlay (overlay){
@@ -165,13 +65,17 @@ function toggleOverlay (overlay){
 
 module.exports.init = () => {
 	getSurveyData().then( surveyData => {
-		const html = buildSurveyHTML(surveyData);
-		const trigger = document.querySelector('.feedback__trigger');
+		const html = surveyBuilder.buildSurvey(surveyData);
+		const trigger = document.querySelector('.feedback__container .feedback__survey-trigger');
+		const isMobile = !!window.getComputedStyle(document.querySelector('.feedback__desktop__promt'))
+																.getPropertyValue('display') !== 'none';
 
 		const feedbackOverlay = new Overlay('feedback-overlay', {
 			html: html,
-			nested: true,
-			parentnode: '.feedback__overlay__container'
+			nested: !isMobile,
+			preventclosing: false,
+			parentnode: isMobile ? 'body' : '.feedback__overlay__container',
+			fullscreen: isMobile
 		});
 
 		trigger.addEventListener('click', () => {
