4d7d61090ce286f5fbf29d589df1d23696886d16
dapenguin <anthony.jeffery@littleblueweb.com>
2018-06-07 14:09:31 +0100

    JSDocs  🐿 v2.8.0

--- src/get-questions.js
+++ src/get-questions.js
@@ -1,3 +1,10 @@
+/**
+ * Takes survey data we have received from the Feedback API, and returns all
+ * the questions in a format needed for posting the survey response.
+ * @param {Array} surveyData The survey data received from the Feedback API.
+ * @returns {Array} An array of questions containing only the info we need for
+ *                  submitting the survey response.
+ */
 function getQuestions (surveyData) {
 	const blocksWithQuestions = surveyData.filter(block => block.questions && block.questions.length > 0);
 	const blockQuestions = [].concat(...blocksWithQuestions.map(block => block.questions));
