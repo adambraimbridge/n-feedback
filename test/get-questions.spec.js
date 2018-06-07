502f3ba1aed3886e03329241c94000f3da8ec21b
dapenguin <anthony.jeffery@littleblueweb.com>
2018-06-07 20:25:21 +0100

    Linting the spacez  🐿 v2.8.0

--- test/get-questions.spec.js
+++ test/get-questions.spec.js
@@ -3,14 +3,14 @@ const getQuestions = require('./../src/get-questions');
 const surveyData = require('./fixtures/survey-data.json');
 
 describe('getQuestions()', () => {
-    const questions = getQuestions(surveyData);
+	const questions = getQuestions(surveyData);
 
-    it('should return an array containing all the questions in the survey', () => {
-        expect(questions).to.be.an('array');
-        expect(questions.length).to.equal(2);
-    });
+	it('should return an array containing all the questions in the survey', () => {
+		expect(questions).to.be.an('array');
+		expect(questions.length).to.equal(2);
+	});
 
-    it('should only include the info we need for the questions in the survey', () => {
-        expect(questions[0]).to.have.all.keys('text', 'id');
-    });
+	it('should only include the info we need for the questions in the survey', () => {
+		expect(questions[0]).to.have.all.keys('text', 'id');
+	});
 });
