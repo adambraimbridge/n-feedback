85fc64ee717b83fe896f9331509befb4d37cde72
dapenguin <anthony.jeffery@littleblueweb.com>
2018-06-08 13:13:53 +0100

    Little tidy up of the HTML indentation  🐿 v2.9.0

--- src/survey-builder.js
+++ src/survey-builder.js
@@ -30,9 +30,11 @@ function buildQuestion (question) {
 }
 
 function buildMultipleChoiceQuestion (question) {
-	const html = [`<fieldset class="feedback__question-radio o-forms">
-									<legend>${question.questionText}</legend>
-									<div class="feedback__question-radio__container o-forms__group">`]; // fieldsets can't display: flex
+	const html = [
+		`<fieldset class="feedback__question-radio o-forms">
+			<legend>${question.questionText}</legend>
+			<div class="feedback__question-radio__container o-forms__group">`
+	]; // fieldsets can't display: flex
 
 	Object.entries(question.choices).forEach( ([choiceId, choice], current, choices) => {
 		let textVisibility = '';
@@ -42,26 +44,31 @@ function buildMultipleChoiceQuestion (question) {
 		}
 		const fieldId = `choice-${choiceId}-${~~(Math.random()*0xffff)}`;
 
-		html.push(`<div class="feedback__question-radio__choice-container">
-								<input type="radio" id="${fieldId}" class="o-forms__radio" name="${question.questionId}" value="${choiceId}" />
-								<label for="${fieldId}" class="o-forms__label ${textVisibility}">
-									<span class="feedback__question-radio-text">${choice.choiceText}</span>
-								</label>
-							</div>`);
+		html.push(
+			`<div class="feedback__question-radio__choice-container">
+				<input type="radio" id="${fieldId}" class="o-forms__radio" name="${question.questionId}" value="${choiceId}" />
+				<label for="${fieldId}" class="o-forms__label ${textVisibility}">
+					<span class="feedback__question-radio-text">${choice.choiceText}</span>
+				</label>
+			</div>`
+		);
 	});
 
-	html.push(`</div>
-						</fieldset>`);
+	html.push(
+			`</div>
+		</fieldset>`
+	);
 	return html.join('\n');
 }
 
 function buildTextEntryQuestion (question) {
 	const fieldId = `text-${question.questionId}-${~~(Math.random()*0xffff)}`;
 
-	const html = `<p class="feedback__question-text-entry o-forms">
-									<label for="${fieldId}" class="o-forms__label">${question.questionText}</label>
-									<textarea id="${fieldId}" name="${question.questionId}" class="o-forms__textarea"></textarea>
-								</p>`;
+	const html =
+		`<p class="feedback__question-text-entry o-forms">
+			<label for="${fieldId}" class="o-forms__label">${question.questionText}</label>
+			<textarea id="${fieldId}" name="${question.questionId}" class="o-forms__textarea"></textarea>
+		</p>`;
 
 	return html;
 }
@@ -78,12 +85,14 @@ function buildSurvey (surveyData) {
 		throw new Error('Bad survey data');
 	}
 
-	const surveyHTML = [`<div class="feedback__survey">
-												<a class="feedback__survey__close-button o-overlay__close" href="#void">
-													<span>I don't want to give feedback</span>
-												</a>
-												<form class="feedback__survey__wrapper-form">
-													<input type="hidden" name="surveyId" value="SV_bECh88bz44kFmq9" />`];
+	const surveyHTML = [
+		`<div class="feedback__survey">
+			<a class="feedback__survey__close-button o-overlay__close" href="#void">
+				<span>I don't want to give feedback</span>
+			</a>
+			<form class="feedback__survey__wrapper-form">
+				<input type="hidden" name="surveyId" value="SV_bECh88bz44kFmq9" />`
+	];
 
 	surveyData.forEach( (block, blockId) => {
 		// Only show the first block initially, hide the others
@@ -102,19 +111,21 @@ function buildSurvey (surveyData) {
 
 		// if this not the last block on the survey
 		if ( surveyData.length - 1 > blockId ) {
-			blockHTML.push(`<p class="feedback__survey__button-bar">
-												<button class="o-buttons o-buttons--primary feedback__survey-next"
-																data-survey-next="feedback__survey-block-${blockId+1}">
-													Next
-												</button>
-											</p>`
+			blockHTML.push(
+				`<p class="feedback__survey__button-bar">
+					<button class="o-buttons o-buttons--primary feedback__survey-next"
+						data-survey-next="feedback__survey-block-${blockId+1}">
+						Next
+					</button>
+				</p>`
 			);
 		} else {
-			blockHTML.push(`<p class="feedback__survey__button-bar">
-												<button class="o-buttons o-buttons--primary feedback__survey-submit">
-													Submit
-												</button>
-											</p>`
+			blockHTML.push(
+				`<p class="feedback__survey__button-bar">
+					<button class="o-buttons o-buttons--primary feedback__survey-submit">
+						Submit
+					</button>
+				</p>`
 			);
 		}
 
