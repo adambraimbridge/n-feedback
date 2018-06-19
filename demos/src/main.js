0ae725902c69c587d0902dd20d80e91f7196420a
Mehmet Duran <mehmet.duran@gmail.com>
2018-06-19 15:30:43 +0100

    Survey submit handler changed to onsubmit
    The survey used to be submitted by an onclick handler on the submit button. We
    changed it to a more elegant onsubmit handler on the form, which works better
    for validation.
    
    - Changed the handler
    - Added app name to initialiser for the demo to work
    
     🐿 v2.8.0

--- demos/src/main.js
+++ demos/src/main.js
@@ -1,3 +1,7 @@
 import feedback from 'n-feedback';
 
+<<<<<<< HEAD
 feedback.init({ name: 'article' });
+=======
+feedback.init({name: 'article'});
+>>>>>>> Survey submit handler changed to onsubmit
