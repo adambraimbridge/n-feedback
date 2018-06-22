e1a723a6672ba2eaed94133fa67e75798cddb193
Mehmet Duran <mehmet.duran@gmail.com>
2018-06-22 12:28:57 +0100

    Adding name to app  🐿 v2.8.0

--- demos/src/main.js
+++ demos/src/main.js
@@ -1,7 +1,3 @@
 import feedback from 'n-feedback';
 
-<<<<<<< HEAD
-feedback.init({ name: 'article' });
-=======
 feedback.init({name: 'article'});
->>>>>>> Survey submit handler changed to onsubmit
